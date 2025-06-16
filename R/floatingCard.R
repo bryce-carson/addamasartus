e3fb5cbe1a7f1d621b3ba0e5ccdb5f9feec6e3ab
Bryce Carson <bcars268@mtroyal.ca>
2025-06-16 00:51:05 -0600

    Move all files from independent R files to Org R chunks

--- R/floatingCard.R
+++
@@ -1,8 +0,0 @@
-floatingCard <- function(..., title = "Title", subtitle = "Subtitle") {
-  shiny::div(class = "content",
-             shiny::div(class = "floating-container-title header", title),
-             shiny::div(class = "meta", subtitle),
-             shiny::div(class = "description", ...)) %>%
-    shiny.semantic::card() |>
-    shiny::div(class = "floating-container")
-}
--- R/parseError.R
+++
@@ -1,4 +0,0 @@
-parseError <- function(name) {
-  msg <- sprintf("The parser parse%sSubrecordData failed during reading.", name)
-  signalCondition(errorCondition(msg, class = "parseError"))
-}
